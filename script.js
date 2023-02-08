37af5458889504c358662b139d0e017d184b8f56
cleberdelima <120141670+cleberdelima@users.noreply.github.com>
2023-02-07 21:13:19 -0300

    gerador de senhas atualizados

---
+++ script.js
@@ -0,0 +1,36 @@
+let sliderElement = document.querySelector("#slider");
+let password      = document.querySelector("#password");
+
+let containerPassword = document.querySelector("#container-password");
+let sizePassword      = document.querySelector("#valor");
+
+//fonte da senha randomica
+let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#';
+let novaSenha = '';
+
+sizePassword.innerHTML = sliderElement.value;
+
+slider.oninput = function() {
+    sizePassword.innerHTML = this.value;
+}
+
+function generatePassword() {
+    let pass = '';
+    for(let i = 0, n = charset.length; i < sliderElement.value; i++) {
+        pass += charset.charAt(Math.floor(Math.random() * n));
+    }
+
+    containerPassword.classList.remove("hide");
+    password.innerHTML = pass;
+    novaSenha          = pass;
+    console.log(pass);
+   
+
+
+}
+
+function copyPassword(){
+    alert("Senha copiada com sucesso!")
+    navigator.clipboard.writeText(novaSenha);
+    }
+
